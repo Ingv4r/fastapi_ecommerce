2529b2dbcc16ee440ee84dcf4f37a316a73d0534
Igor <venom3800@yandex.ru>
2025-03-25 08:11:12 +0300

    Add auth with JWT token

--- app/routers/auth.py
+++ app/routers/auth.py
@@ -1,5 +1,8 @@
+import os
+import jwt
+
 from fastapi import APIRouter, Depends, status, HTTPException
-from fastapi.security import HTTPBasic, HTTPBasicCredentials
+from fastapi.security import HTTPBasic, OAuth2PasswordBearer, OAuth2PasswordRequestForm
 from sqlalchemy import select, insert
 from typing import Annotated
 from sqlalchemy.ext.asyncio import AsyncSession
@@ -8,11 +11,87 @@ from passlib.context import CryptContext
 from app.models.user import User
 from app.schemas import CreateUser
 from app.backend.db_depends import get_db
+from dotenv import load_dotenv
+from datetime import datetime, timedelta, timezone
 
+
+load_dotenv()
 
 router = APIRouter(prefix="/auth", tags=["auth"])
 bcrypt_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="auth/token")
 security = HTTPBasic()
+
+SECRET_KEY = os.environ.get("SECRET_KEY")
+ALGORITHM = os.environ.get("ALGORITHM")
+
+
+async def authenticate_user(db: Annotated[AsyncSession, Depends(get_db)], username: str, password: str):
+    user: User = await db.scalar(select(User).where(User.username == username))
+    if not user or not bcrypt_context.verify(password, user.hashed_password) or not user.is_active:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Invalid authentication credentials",
+            headers={"WWW-Authenticate": "Bearer"}
+        )
+    return user
+
+
+async def create_access_token(
+        username: str, user_id: int, is_admin: bool,
+        is_superuser: bool, is_customer: bool, expires_delta: timedelta):
+
+    exp = datetime.now(timezone.utc) + expires_delta
+    exp = int(exp.timestamp())
+
+    payload = {
+        "sub": username,
+        "user_id": user_id,
+        "is_admin": is_admin,
+        "is_superuser": is_superuser,
+        "is_customer": is_customer,
+        "exp": exp
+    }
+
+    return jwt.encode(payload, SECRET_KEY, algorithm=ALGORITHM)
+
+
+async def get_current_user(token: Annotated[str, Depends(oauth2_scheme)]):
+
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        username: str | None = payload.get("sub")
+        user_id: int | None = payload.get("id")
+        is_admin: bool | None = payload.get("is_admin")
+        is_superuser: bool | None = payload.get("is_superuser")
+        is_customer: bool | None = payload.get("is_customer")
+        expire: int | None = payload.get("exp")
+
+        if username is None or user_id is None:
+            raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Could not validate user")
+        if expire is None:
+            raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="No access token supplied")
+        if not isinstance(expire, int):
+            raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Invalid token format")
+
+        current_time = datetime.now(timezone.utc).timestamp()
+
+        if expire < current_time:
+            raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Token expired!")
+
+        return {
+            "username": username,
+            "id": user_id,
+            "is_admin": is_admin,
+            "is_superuser": is_superuser,
+            "is_customer": is_customer,
+        }
+
+    except jwt.ExpiredSignatureError:
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Token expired!")
+
+    except jwt.exceptions:
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Could not validate user")
 
 
 @router.post("/", status_code=status.HTTP_201_CREATED)
@@ -31,16 +110,23 @@ async def create_user(db: Annotated[AsyncSession, Depends(get_db)], create_user:
     }
 
 
-async def get_current_username(
+@router.post("/token", status_code=status.HTTP_200_OK)
+async def login(
         db: Annotated[AsyncSession, Depends(get_db)],
-        credentials: HTTPBasicCredentials = Depends(security)
-):
-    user = await db.scalar(select(User).where(User.username == credentials.username))
-    if not user or not bcrypt_context.verify(credentials.password, user.hashed_password):
-        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Invalid credentials")
-    return user
+        form_data: Annotated[OAuth2PasswordRequestForm, Depends()]):
+
+    user = await authenticate_user(db, form_data.username, form_data.password)
+    token = await create_access_token(
+        user.username, user.id, user.is_admin, user.is_supplier,
+        user.is_customer, expires_delta=timedelta(minutes=20)
+    )
+
+    return {
+        "access_token": token,
+        "token_type": "bearer"
+    }
 
 
-@router.get("/users/me", status_code=status.HTTP_200_OK)
-async def get_current_user(user: dict = Depends(get_current_username)):
-    return {"user": user}
+@router.get("/read_current_user")
+async def read_current_user(current_user: User = Depends(get_current_user)):
+    return current_user
--- app/schemas.py
+++ app/schemas.py
@@ -21,3 +21,14 @@ class CreateUser(BaseModel):
     username: str
     email: str
     password: str
+
+
+class GetCurrentUser(BaseModel):
+    id: int
+    first_name: str
+    last_name: str
+    username: str
+    email: str
+    is_admin: bool
+    is_customer: bool
+    is_supplier: bool
